2ca651ad264be9336b274c914782cf7c8f56023e
L1edge <koroliuk.artur@student.uzhnu.edu.ua>
2025-07-09 15:35:57 +0300

    1.0.0

---
+++ structure/header.js
@@ -0,0 +1,18 @@
+import './main.css';
+
+function Header() {
+    return(
+        <div className='head-zone'>
+            <header className='header'>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </header>
+
+        </div>
+    )
+}
+
+export default Header;
